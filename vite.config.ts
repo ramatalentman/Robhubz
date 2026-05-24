7669c37b43ece8415160b58aadc4a3d61b255381
ramatalentman <ramatalentman@gmail.com>
2026-05-24 10:44:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-
+<script async="async" data-cfasync="false" src="https://pl29536214.effectivecpmnetwork.com/65fe59d5303a63f996c53206702d0102/invoke.js"></script>
+<div id="container-65fe59d5303a63f996c53206702d0102"></div>
+  
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
